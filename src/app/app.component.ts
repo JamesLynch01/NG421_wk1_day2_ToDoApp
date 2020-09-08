27f2b7bc9e55ea92d03c9c9ec8abef07a17f6765
JamesLynch01 <Jamesblynch94@gmail.com>
2020-09-08 15:24:34 -0500

    I finish it late

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'NG421wk1day1ToDoApp';
+  title = 'Todos';
+  todoList: any [] = [];
+  todoTitle: string;
+  
+  ngOnInit() {
+    this.todoTitle = '';
+    this.todoList = [
+      // example of how to make an item in todo list
+      { title: 'Install Angular CLI', isDone: false },
+    
+    ];
+  }
+
+  addTodo():void {
+    this.todoList.push({
+      title: this.todoTitle,
+      isDone: false
+    });
+
+    this.todoTitle = '';
+  }
+
+  deleteTodo(todo:any){ 
+    const index = this.todoList.findIndex(todoItem => todoItem === todo);
+      this.todoList.splice(index, 1);
+  }
 }
+
+
